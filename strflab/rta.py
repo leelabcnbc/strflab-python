8e77b85622359a343b849d1a7239e7167c9558f3
Yimeng Zhang <zym1010@gmail.com>
2017-06-09 13:49:58 -0400

    add naive version of spike trigger average.

--- strflab/rta.py
+++ strflab/rta.py
@@ -34,7 +34,8 @@ def rta(stimulus_list, response_list, config=None):
         'regularization_penalty': 0.1,  # alpha in Ridge Regression, or lasso.
         'solver_pars': {},  # other pars passed into solver.
         'delays': [0],  # what delay line to use. By default, a zero delay kernel,
-        'truncate': (0, 0)  # first number is samples to drop for each trial in the front, second in the back.
+        'truncate': (0, 0),  # first number is samples to drop for each trial in the front, second in the back.
+        'correction': True,  # having correction, by doing linear regression (pseudo inverse).
     }
     config_actual.update(config)
     # TODO: check that your config is reall a valid one.
@@ -54,31 +55,37 @@ def rta(stimulus_list, response_list, config=None):
     n_stimulus_all, _ = stimulus_flat_all.shape
     response_all = np.concatenate([truncate_array(resp, truncate_config) for resp in response_list], axis=0)
 
-    # ok. let's do the regression.
-    additional_pars = config_actual['solver_pars']
-    if not config_actual['regularization']:
-        classifier = LinearRegression(copy_X=False, **additional_pars)
-    else:
-        if config_actual['regularization'] == 'ridge':
-            alpha = config_actual['regularization_penalty'] * n_stimulus_all
-            # multiply to make alpha independent of sample size.
-            # see <http://scikit-learn.org/stable/modules/linear_model.html#ridge-regression>
-            # I also tested this myself.
-            # for example, try duplicate X and y in
-            # <http://scikit-learn.org/stable/auto_examples/linear_model/plot_ridge_path.html>
-            # and see the change in plot; if I also make alpha = 2*alpha, then plot stays the same.
-            classifier = Ridge(alpha=alpha, copy_X=False, **additional_pars)
-        elif config_actual['regularization'] == 'lasso':
-            # this is because in lasso, alpha is against every sample already.
-            # check <http://scikit-learn.org/stable/modules/linear_model.html#lasso>
-            alpha = config_actual['regularization_penalty']
-            classifier = Lasso(alpha=alpha, copy_X=False, **additional_pars)
+    if config_actual['correction']:
+        # ok. let's do the regression.
+        additional_pars = config_actual['solver_pars']
+        if not config_actual['regularization']:
+            classifier = LinearRegression(copy_X=False, **additional_pars)
         else:
-            raise RuntimeError('not support regularization yet!')
+            if config_actual['regularization'] == 'ridge':
+                alpha = config_actual['regularization_penalty'] * n_stimulus_all
+                # multiply to make alpha independent of sample size.
+                # see <http://scikit-learn.org/stable/modules/linear_model.html#ridge-regression>
+                # I also tested this myself.
+                # for example, try duplicate X and y in
+                # <http://scikit-learn.org/stable/auto_examples/linear_model/plot_ridge_path.html>
+                # and see the change in plot; if I also make alpha = 2*alpha, then plot stays the same.
+                classifier = Ridge(alpha=alpha, copy_X=False, **additional_pars)
+            elif config_actual['regularization'] == 'lasso':
+                # this is because in lasso, alpha is against every sample already.
+                # check <http://scikit-learn.org/stable/modules/linear_model.html#lasso>
+                alpha = config_actual['regularization_penalty']
+                classifier = Lasso(alpha=alpha, copy_X=False, **additional_pars)
+            else:
+                raise RuntimeError('not support regularization yet!')
 
-    classifier.fit(stimulus_flat_all, response_all)
-    fit_kernels = classifier.coef_
-    fit_intercept = classifier.intercept_
+        classifier.fit(stimulus_flat_all, response_all)
+        fit_kernels = classifier.coef_
+        fit_intercept = classifier.intercept_
+    else:
+        fit_intercept = np.zeros(n_neuron, dtype=np.float64)
+        # this case, the intercept doesn't matter, it's just dummy.
+        # then doing naive averaging.
+        fit_kernels = np.dot(response_all.T, stimulus_flat_all) / n_stimulus_all
 
     assert fit_intercept.shape == (n_neuron,)
     assert fit_kernels.shape == (n_neuron, np.product(kernel_shape_each_delay) * len(delays))
